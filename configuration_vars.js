a3b7e5147394bd3cc842d68bbd74c5fbbbea90a9
John Haugeland <stonecypher@gmail.com>
2015-01-15 14:47:18 -0800

    conf vars

---
+++ configuration_vars.js
@@ -0,0 +1,13 @@
+
+module.exports = [
+  "base-font-size",
+  "base-line-height",
+  "rhythm-unit",
+  "rem-with-px-fallback",
+  "default-rhythm-border-width",
+  "default-rhythm-border-style",
+  "round-to-nearest-half-line",
+  "min-line-padding",
+  "base-leader",
+  "base-half-leader"
+];
